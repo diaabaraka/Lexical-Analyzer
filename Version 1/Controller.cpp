d987345bad4b10edc8fc8e6bf15007d2f0d64aed
Waleed Adel <waleed.adel.mahmoud@gmail.com>
2015-04-18 00:04:56 +0200

    modified controller

--- Version 1/Controller.cpp
+++ Version 1/Controller.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include"DFA.h"
 #include "Tranzation.h"
+#include "Follow.h"
 #include "DfaMinimizer.h"
 #include "ParsingTable.h"
 #include "StartToParser.h"
@@ -78,24 +79,35 @@ int main()
 
   /////////////////////////////////////////////////////////////////////////
 
-  StartToParser* startToParser =new StartToParser();
+  StartToParser * startToParser =new StartToParser();
 
   string startSymbol =startToParser->startingSymbol ;  // diaa needs it
 vector<string>tokens=tranzation->tokensForParser;
 
   set<string>Terminals = startToParser->terminals; // diaa , waleed  , mostafa and mahmoud need it
 
- // StartToParser.fillGrammerList();   //to fill the multimap
+  // StartToParser.fillGrammerList();   //to fill the multimap
 
   /////////////////////////////////////////////////////////////////////
 
+multimap<string, string>rules = startToParser->grammers;
+
+  Follow * f=new Follow();
+  multimap <string, string >follow_map =  f->getAllFollow();
 
 
+  First * firstobj=new First();
+ multimap<string , string> first_map = firstobj->getAllFirst();
 
-//   ParsingTable* pt=new ParsingTable(rules,first_map,follow_map,allNonTerminals);  // parsing table waleeeeed
 
-//   map<string, map<string,string> > parsing_table=pt.getFinalTable()  // from waleed to diaa 7awell from waleed to diaa 7awell hal tasm3ony ?
+   ParsingTable* pt=new ParsingTable(rules,first_map,follow_map,Terminals);  // parsing table waleeeeed
+
+    map<string, map<string,string> > parsing_table=pt->getFinalTable();  // from waleed to diaa 7awell from waleed to diaa 7awell hal tasm3ony ?
+
+  // go ya diaa ^
+
 
 
     return 0;
+
 }
